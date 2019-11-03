5decbb3830ea59c14323de7d42887e7d84f67bd5
Thomas J. Reese <treese@spedtrack.com>
2019-11-03 08:32:28 -0600

    fix

--- server.js
+++ server.js
@@ -192,7 +192,7 @@ function getDistance(currLatLong, jobLatLong, callback) {
         units: 'imperial',
         origins: currLatLong,
         destinations: jobLatLong,
-        mode: transit|bicycling|walking|driving
+        mode: 'transit|bicycling|walking|driving'
     }, function (err, response) {
 
         if (!err) {
