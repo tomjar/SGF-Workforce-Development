b40fceafa79fb9bbc682f04467f4609d9735c0a6
tomjar <beholdersortof@yahoo.com>
2019-11-04 21:55:05 -0600

    more posix signals

--- server.js
+++ server.js
@@ -258,5 +258,9 @@ function getDistances (origin, destinations, callback) {
   })
 }
 
-process.on('SIGINT', shutdown)
+process.on('SIGHUP', shutdown)
+process.on('SIGKILL', shutdown)
+process.on('SIGSTOP', shutdown)
 process.on('SIGTERM', shutdown)
+process.on('SIGCHLD', shutdown)
+process.on('SIGABRT', shutdown)
